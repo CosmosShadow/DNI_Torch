851e341fb8d2e25608642e3db8d63f6b687186bb
lichen <lichenarthurml@gmail.com>
2016-08-30 23:17:32 +0800

    rm confidence

--- DNI.lua
+++ DNI.lua
@@ -5,7 +5,7 @@ function DNI:__init(src_model, M, M_criterion)
    self.src_model = src_model
    self.M = M
    self.M_criterion = M_criterion or nn.MSECriterion()
-   self.current_M_confidence = 0
+   self.current_M_confidence = 0.0001
 end
 
 function DNI:updateOutput(input)
@@ -23,19 +23,6 @@ function DNI:updateGradInput(input, gradOutput)
    local M_error = self.M_criterion:forward(self.SyntheticGradients, gradOutput) / self.SyntheticGradients:nElement()
    local M_grad = self.M_criterion:backward(self.SyntheticGradients, gradOutput)
    self.M:backward(self.output, M_grad)
-
-   M_error = M_error*1e7
-   -- print(M_error)
-
-   if M_error < 1 then
-      self.current_M_confidence = (1 - M_error)*0.0003
-   else
-      self.current_M_confidence = 0
-   end
-
-   -- print(self.current_M_confidence)
-
-   -- self.gradInput = self.src_model:backward(input, gradOutput)
 
    return self.gradInput
 end
